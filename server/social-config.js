288d98cfb16c137580ba271f0014ff3c50733b0c
Austin Chen <austinchen88@D-10-65-22-6.dhcp4.washington.edu>
2015-10-18 02:38:07 -0700

    Fb Login

---
+++ server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+ 
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '1636071813346894',
+    secret: '59795f5000573ac1f560fc4f4afd04ec'
+});
