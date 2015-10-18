9bf1e024cb80a756cdd8c7535ba5a488ea264dae
Austin Chen <austinchen88@D-10-65-22-6.dhcp4.washington.edu>
2015-10-18 05:50:31 -0700

    single instance lists

--- client/templates/facebook-login.js
+++ client/templates/facebook-login.js
@@ -4,6 +4,26 @@ Template.flogin.events({
             if (err) {
                 throw new Meteor.Error("Facebook login failed");
             }
+
+            if(Lists.find({userId: Meteor.userId(), name: "My Statuses"}).count() != 1) {
+                var statusList = {name: 'My Statuses', incompleteCount: 0};
+                statusList._id = Lists.insert(statusList);
+                Lists.update(statusList._id, {$set: {userId: Meteor.userId()}});
+            }
+
+            if(Lists.find({userId: Meteor.userId(), name: "My Goals"}).count() < 1) {
+                var goalsList = {name: 'My Goals', incompleteCount: 0};
+                goalsList._id = Lists.insert(goalsList);
+                Lists.update(goalsList._id, {$set: {userId: Meteor.userId()}});
+            }
+
+            if(Lists.find({userId: Meteor.userId(), name: "Ransome Pot"}).count() < 1) {
+                var ransomeList = {name: 'Ransome Pot', incompleteCount: 0};
+                ransomeList._id = Lists.insert(ransomeList);
+                Lists.update(ransomeList._id, {$set: {userId: Meteor.userId()}});
+            }
+
+
             Router.go('home');
         });
     },
