4d2e04cb77903aff9ac8bc808796511d0a51d458
Austin Chen <austinchen88@D-10-65-22-6.dhcp4.washington.edu>
2015-10-18 06:27:17 -0700

    remove ability to delete list

--- client/templates/lists-show.js
+++ client/templates/lists-show.js
@@ -59,44 +59,6 @@ var saveList = function(list, template) {
   Lists.update(list._id, {$set: {name: template.$('[name=name]').val()}});
 }
 
-var deleteList = function(list) {
-  // ensure the last public list cannot be deleted.
-  if (! list.userId && Lists.find({userId: {$exists: false}}).count() === 1) {
-    return alert("Sorry, you cannot delete the final public list!");
-  }
-
-  var message = "Are you sure you want to delete the list " + list.name + "?";
-  if (confirm(message)) {
-    // we must remove each item individually from the client
-    Todos.find({listId: list._id}).forEach(function(todo) {
-      Todos.remove(todo._id);
-    });
-    Lists.remove(list._id);
-
-    Router.go('home');
-    return true;
-  } else {
-    return false;
-  }
-};
-
-var toggleListPrivacy = function(list) {
-  if (! Meteor.user()) {
-    return alert("Please sign in or create an account to make private lists.");
-  }
-
-  if (list.userId) {
-    Lists.update(list._id, {$unset: {userId: true}});
-  } else {
-    // ensure the last public list cannot be made private
-    if (Lists.find({userId: {$exists: false}}).count() === 1) {
-      return alert("Sorry, you cannot make the final public list private!");
-    }
-
-    Lists.update(list._id, {$set: {userId: Meteor.userId()}});
-  }
-};
-
 Template.listsShow.events({
   'click .js-cancel': function() {
     Session.set(EDITING_KEY, false);
